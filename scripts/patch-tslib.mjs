82586728f4273ef789862da41b432bc4be50da9e
Jorge Afonso <83745669+jamffer@users.noreply.github.com>
2026-06-09 01:04:35 -0300

    fix: add __extends to patch-tslib.mjs

--- scripts/patch-tslib.mjs
+++ scripts/patch-tslib.mjs
@@ -17,6 +17,29 @@ const LIBS_DIR = join(
 
 // Inline replacement for tslib's __rest helper
 const TSLIB_INLINE = `
+var __extendStatics = function(d, b) {
+    __extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+    return __extendStatics(d, b);
+};
+function __extends(d, b) {
+    if (typeof b !== "function" && b !== null)
+        throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+    __extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    }
+    return __assign.apply(this, arguments);
+};
 const __rest = (s, e) => {
   const t = {};
   for (const p in s)
